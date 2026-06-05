6f484f99e841ca739131eb509e945c2531a7afa6
GLB25112fw008 <manshimaurya5544@gmail.com>
2026-06-05 21:56:23 +0530

    Day-4 uploaded

---
+++ Day-4/Day4-qn15.cpp
@@ -0,0 +1,25 @@
+//Check armstrong number
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    int num,ln=0,n,sum=0,n1;
+    cout<<"Enter the number:";
+    cin >>num;
+    n=num;
+    while(n>0){
+        n=n/10;
+        ln++;
+    }
+    n1=num;
+    for (int i=0;i<ln;i++){
+        int a=n1%10;
+        sum=sum+pow(a,ln);
+        n1=n1/10;
+    }
+        if (num==sum){
+        cout << num <<" is an armstrong number ";
+        }
+        else {
+        cout << num <<" is not an armstrong number" ;
+        }
+}
